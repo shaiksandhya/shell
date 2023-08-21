bf2eef903304e269f7fe842fee85ef55ef54b4ba
Sandhya <shaiksandhya60@gmail.com>
2023-08-21 19:27:18 +0530

    first commit

--- 01.helloworld.sh
+++ 01.helloworld.sh
@@ -4,7 +4,7 @@ echo "i learning devops and AWS"
 echo "hi"
 
 PERSON1=Ramesh
-PERSON2=Suresh
+PERSON2=Rohit
 
 echo "$PERSON1: Hi $PERSON2, good mrng"
 echo "Suresh: Hi $PERSON1, i am fine. what about you"
