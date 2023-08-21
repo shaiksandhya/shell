fc6535725389ac90745b24ad7e6fc93ac9005828
Sandhya <shaiksandhya60@gmail.com>
2023-08-21 19:48:02 +0530

    first commit

---
+++ 05.variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "pls enter ur username"
+
+read  USERNAME 
+
+echo "usename entered is : $USERNAME"
+
+echo "pls enter ur password"
+
+read PASSWORD
+
+echo "password entered is : $PASSWORD"
