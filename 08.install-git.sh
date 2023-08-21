d5f9ff464410ef891cd6428290e404cd7eeb0f63
Sandhya <shaiksandhya60@gmail.com>
2023-08-21 20:33:01 +0530

    first commit

--- 08.install-git.sh
+++ 08.install-git.sh
@@ -2,12 +2,13 @@
 
 USERID=($id -u)
 
-if [ $USERID -NE 0]
+if [ $USERID -NE 0 ]
 then
     echo "Error:: pls run the script with root access"
     exit 1
-else
-    echo "info::you are root user"
+#else
+  # echo "info::you are root user"
 fi
 
 yum install mysql -y
+
