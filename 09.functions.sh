ff93b2c7b97934842898a4d806c168c841d3ba6f
Sandhya <shaiksandhya60@gmail.com>
2023-08-21 20:59:21 +0530

    first commit

--- 09.functions.sh
+++ 09.functions.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
+DATE= $(date +%F-%H-%M-%S)
+SCRIPT-NAME= $0
+LOGFILE= /tmp/$SCRIPT-NAME.$DATE.log
 USERID=$(id -u)
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
         echo "$2...Failure"
+        exit 1
     else
         echo "$2...Success"
 fi
