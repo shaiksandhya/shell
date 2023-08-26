85c9646f1be672bcc4fa9831d9b7de24532e2d96
Sandhya <shaiksandhya60@gmail.com>
2023-08-26 07:56:31 +0530

    msg

--- 09.functions.sh
+++ 09.functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-DATE= $(date +%F-%H-%M-%S)
+DATE= $(date +%F)
 SCRIPT_NAME=$0
-LOGFILE= /tmp/$SCRIPT_NAME.$DATE.log
+LOGFILE= /tmp/$SCRIPT_NAME-$DATE.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
