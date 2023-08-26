c0ed39e35365dd943127a0853f9e5810958baeca
Sandhya <shaiksandhya60@gmail.com>
2023-08-26 08:31:58 +0530

    msg

--- 12.loops.sh
+++ 12.loops.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error:: pls run the script with root access $N"
+    exit 1
+fi
+
 
 for i in $@
 do
