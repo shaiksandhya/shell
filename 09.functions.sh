63c76f0d4bca1563c1c8a2dcf0db36b1cbae3d13
Sandhya <shaiksandhya60@gmail.com>
2023-08-26 08:09:16 +0530

    msg

--- 09.functions.sh
+++ 09.functions.sh
@@ -5,13 +5,17 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2...Failure"
+        echo "$2...$R Failure $N"
         exit 1
     else
-        echo "$2...Success"
+        echo "$2...$$G Success $N"
 fi
 }
 
