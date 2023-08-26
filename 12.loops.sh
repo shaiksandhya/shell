8e021294f6c6369a2b992d36a6bc4efc123c9f5f
Sandhya <shaiksandhya60@gmail.com>
2023-08-26 08:52:58 +0530

    msg

--- 12.loops.sh
+++ 12.loops.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+DATE=$(date +%F)
+LOGDIR=/home/centos/shell.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$0-$DATE.log
 USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 if [ $USERID -ne 0 ]
 then
@@ -12,8 +16,24 @@ then
     exit 1
 fi
 
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2...$R Failure $N"
+        exit 1
+    else
+     echo -e " Installing $2... $G Success $N"
+   fi   
+}
 
 for i in $@
 do
-    yum install $i -y
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then    
+        echo  "$i is not installed, let's install it"
+    yum install $i -y &>>$LOGFILE
+    VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
 done
